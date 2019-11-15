1a66f2f3414563d2b94356400bae5fcb4e80c071
Mikerah <mikerahqc@protonmail.com>
2019-11-15 13:53:44 -0500

    Added subrepo and moved compiled wasm file

--- src/client_bindings.ts
+++ src/client_bindings.ts
@@ -1,8 +1,9 @@
 import {readFileSync} from "fs";
 
 async function createWebAssembly() {
-    const buf = readFileSync("../client.wasm");
+    const buf = readFileSync("../compiled_wasm/client.wasm");
     const res = await WebAssembly.instantiate(new Uint8Array(buf).buffer);  
     console.log(res);
 }
+
 
